918d489343ef1b671dc2fec5f063eebaee706a62
MikhailYurkov <9906205@gmail.com>
2022-01-13 23:42:08 +0300

    zadanie 5

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "фруктовая")
+{
+    Console.WriteLine("Привет Мила!");
+}
+else
+{
+    Console.Write("Привет Юзер ");
+    Console.WriteLine(username);
+}
